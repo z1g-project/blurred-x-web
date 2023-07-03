d98a3f8e758077f5f0b9d0a4a0bd5932dd749152
John Glynn <playingallday383@gmail.com>
2023-07-03 16:53:55 -0400

    UV Configuration Change

--- dist/uv/uv.config.js
+++ dist/uv/uv.config.js
@@ -1,53 +1,13 @@
-/**
- * @typedef {(encoded: string) => string} UVEncode
- */
+// This file overwrites the stock UV config.js
 
-/**
- * @typedef {(encoded: string) => string} UVDecode
- */
-
-/**
- * @typedef {{ encode: UVEncode, decode: UVDecode }} UVCodec
- */
-
-/**
- * @typedef {{ codec: Record<string, { codec: Record<string, UVCodec> } }} Ultraviolet
- */
-
-/**
- * @typedef {{ bare: string; prefix: string; handler: string; bundle: string; config: string; sw: string; encodeUrl: UVEncode; decodeUrl: UVDecode }} UVConfig
- */
-
-{
-	/** @type {Ultraviolet} */
-	const Ultraviolet = globalThis.Ultraviolet;
-
-	/**
-	 *
-	 * @param {string} env
-	 */
-	const format = (env) => {
-		const { host, hostname, protocol } = globalThis.location;
-
-		return env
-			.replace('%{location.host}', host)
-			.replace('%{location.hostname}', hostname)
-			.replace('%{location.protocol}', protocol);
-	};
-
-	/**
-	 * @type {UVConfig}
-	 */
-	const config = {
-		prefix: "" + '/uv/service/',
-		bare: "https://uv.studentportal.lol/",
-		encodeUrl: Ultraviolet.codec.xor.encode,
-		decodeUrl: Ultraviolet.codec.xor.decode,
-		handler: "" + '/uv/uv.handler.js',
-		bundle: "" + '/uv/uv.bundle.js',
-		config: "" + '/uv/uv.config.js',
-		sw: "" + '/uv/uv.sw.js',
-	};
-
-	globalThis.__uv$config = config;
-}
+self.__uv$config = {
+    prefix: "/uv/service/",
+    bare: "https://uv.studentportal.lol/",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
+  };
